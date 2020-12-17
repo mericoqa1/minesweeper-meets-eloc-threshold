14119cb775ad51cdbad9e17acfacc9d46f535852
mericoqa1 <github4merico+mericoqa1@s.gonze.com>
2020-12-17 09:56:15 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+## Criteria for minesweeper
+
+When a function's cyclomatic complexity goes from above or equals to 10 to below 10.
+And the repo has at least 1,000 ELOCs
+
+## Test cases
+
+1. test1 has an intial cc of 11, and goes down to 9 in the second commit
+2. test2 has an intial cc of 10, and goes down to 9 in the second commit
---
+++ main.py
@@ -0,0 +1,42 @@
+def test1():
+    if a:
+        return a
+    elif b:
+        return b
+    elif c:
+        return c
+    elif d:
+        return d
+    elif e:
+        return e
+    elif f:
+        return f
+    elif g:
+        return g
+    elif h:
+        return h
+    elif i:
+        return i
+    elif j:
+        return j
+
+
+def test2():
+    if a:
+        return a
+    elif b:
+        return b
+    elif c:
+        return c
+    elif d:
+        return d
+    elif e:
+        return e
+    elif f:
+        return f
+    elif g:
+        return g
+    elif h:
+        return h
+    elif i:
+        return i
